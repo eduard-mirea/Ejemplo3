d5014ddf636b939d175ec8fd1e3e3be075e80306
eduard-mirea <alumir79@ieselcaminas.org>
2022-10-04 22:50:44 +0200

    Initial commit

---
+++ src/Ejemplo3.java
@@ -0,0 +1,15 @@
+    import java.util.Scanner;
+public class Ejemplo3 {
+    public static void main(String[] args) {
+        float x;
+        float squareArea;
+        System.out.println("Enter the side:");
+
+        //Reading the value
+        Scanner inputValue;
+        inputValue = new Scanner(System.in);
+        x = inputValue.nextFloat();
+        squareArea =x*x;
+        System.out.println(squareArea);
+    }
+}
